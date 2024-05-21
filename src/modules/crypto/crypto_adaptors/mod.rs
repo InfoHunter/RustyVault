50ff3779bcd08730429c3c091bf6eb8dc1067bfd
Paul Yang <kaishen.yy@alipay.com>
2024-05-21 17:01:01 +0800

    Support cryptography module

---
+++ src/modules/crypto/crypto_adaptors/mod.rs
@@ -0,0 +1,11 @@
+//! This is a Rust module that contains several adaptors to different cryptography libraries.
+//! The rusty_vault::crypto module utilize these adaptors to do the real crypto operations.
+//!
+//! Only one crypto adaptor can be used in one build. It's configured when building RustyVault.
+//! An adaptor implements a set of methods that perform cryptograhpy operations like encryption,
+//! decription, signing, verification and so on.
+
+#[cfg(feature = "crypto_adaptor_openssl")]
+pub mod openssl_adaptor;
+#[cfg(feature = "crypto_adaptor_tongsuo")]
+pub mod tongsuo_adaptor;
---
+++ src/modules/crypto/crypto_adaptors/openssl_adaptor.rs
@@ -0,0 +1,326 @@
+//! This is the OpenSSL adaptor.
+
+use crate::errors::RvError;
+use crate::modules::crypto::{AEADCipher, AESKeySize, BlockCipher, CipherMode, AES};
+use openssl::symm::{Cipher, Crypter, Mode, encrypt, encrypt_aead, decrypt, decrypt_aead};
+use openssl::rand::rand_priv_bytes;
+
+pub struct AdaptorCTX {
+    ctx: Crypter,
+    tag_set: bool,
+    aad_set: bool,
+}
+
+impl AES {
+    /// This function is the constructor of the AES struct, it returns a new AES object on success.
+    ///
+    /// keygen: true stands for generating a key and iv; if false, then the caller needs to feed in
+    /// the specific key and iv values through the parameters.
+    /// size: bit-length of AES. If omitted, AESKeySize::AES128 is used as default.
+    /// mode: cipher mode of AES, such as CBC, GCM, etc. If omitted, CipherMode::CBC is default.
+    /// key: symmetric key that is used to encrypt and decrypt data.
+    /// iv: initialization vector. This depends on specific mode, for instance, ECB requires no IV.
+    pub fn new(
+        keygen: bool,
+        size: Option<AESKeySize>,
+        mode: Option<CipherMode>,
+        key: Option<Vec<u8>>,
+        iv: Option<Vec<u8>>,
+    ) -> Result<Self, RvError> {
+        // default algorithm: AES128 + CBC.
+        let mut k_size = AESKeySize::AES128;
+        let mut c_mode = CipherMode::CBC;
+        let aes_key: Vec<u8>;
+        let aes_iv: Vec<u8>;
+
+        if let Some(x) = size {
+            k_size = x;
+        }
+
+        if let Some(y) = mode {
+            c_mode = y;
+        }
+
+        if keygen == false {
+            match (key, iv) {
+                (Some(x), Some(y)) => {
+                    aes_key = x.clone();
+                    aes_iv = y.clone();
+                },
+                _ => return Err(RvError::ErrCryptoCipherInitFailed),
+            }
+        } else {
+            // generate new key and iv based on k_size.
+            match k_size {
+                AESKeySize::AES128 => {
+                    // for aes-128, key is 16 bytes and iv is 16 bytes
+                    let mut buf = [0; 16];
+                    let mut buf2 = [0; 16];
+                    rand_priv_bytes(&mut buf).unwrap();
+                    aes_key = buf.to_vec();
+                    rand_priv_bytes(&mut buf2).unwrap();
+                    aes_iv = buf2.to_vec();
+                },
+                AESKeySize::AES256 => {
+                    // for aes-256, key is 32 bytes and iv is 16 bytes
+                    let mut buf = [0; 32];
+                    let mut buf2 = [0; 16];
+                    rand_priv_bytes(&mut buf).unwrap();
+                    aes_key = buf.to_vec();
+                    rand_priv_bytes(&mut buf2).unwrap();
+                    aes_iv = buf2.to_vec();
+                },
+            }
+        }
+
+        Ok (
+            AES {
+                alg: (k_size, c_mode),
+                key: aes_key,
+                iv: aes_iv,
+                aad: None,
+                ctx: None,
+                tag: None,
+            }
+        )
+    }
+
+    /// This function returns the key and iv vaule stored in one AES object.
+    ///
+    /// Two values are returned in a tuple: the first element represents the key, and the second
+    /// element represents the IV. Elements may be None if unset.
+    pub fn get_key_iv(&self) -> (Vec<u8>, Vec<u8>) {
+        (self.key.clone(), self.iv.clone())
+    }
+}
+
+impl BlockCipher for AES {
+    fn encrypt(&mut self, plaintext: &Vec<u8>) -> Result<Vec<u8>, RvError> {
+        match self.alg {
+            (AESKeySize::AES128, CipherMode::CBC) => {
+                let ciphertext = encrypt(
+                    Cipher::aes_128_cbc(),
+                    &self.key,
+                    Some(&self.iv),
+                    plaintext).unwrap();
+                return Ok(ciphertext.to_vec());
+            }
+            (AESKeySize::AES128, CipherMode::GCM) => {
+                // aes_128_gcm's tag is 16-bytes long.
+                let tag: &mut [u8] = &mut [0; 16];
+                let ciphertext = encrypt_aead(
+                    Cipher::aes_128_gcm(),
+                    &self.key,
+                    Some(&self.iv),
+                    &self.aad.clone().unwrap(),
+                    plaintext,
+                    tag
+                    ).unwrap();
+                self.tag = Some(tag.to_vec());
+                return Ok(ciphertext.to_vec());
+            }
+            _ => Err(RvError::ErrCryptoCipherOPNotSupported),
+        }
+    }
+
+    fn encrypt_update(&mut self, plaintext: Vec<u8>, ciphertext: &mut Vec<u8>
+        ) -> Result<usize, RvError> {
+        let cipher;
+
+        match self.alg {
+            (AESKeySize::AES128, CipherMode::CBC) => {
+                cipher = Cipher::aes_128_cbc();
+            }
+            (AESKeySize::AES128, CipherMode::GCM) => {
+                cipher = Cipher::aes_128_gcm();
+            }
+            _ => { return Err(RvError::ErrCryptoCipherOPNotSupported); }
+        }
+
+        if let None = self.ctx {
+            // init adaptor ctx if it's not inited.
+            let encrypter = Crypter::new(
+                cipher,
+                Mode::Encrypt,
+                &self.key,
+                Some(&self.iv)
+            ).unwrap();
+            let adaptor_ctx = AdaptorCTX { ctx: encrypter, tag_set: false, aad_set: false };
+
+            self.ctx = Some(adaptor_ctx);
+        }
+
+        if self.alg.1 == CipherMode::GCM || self.alg.1 == CipherMode::CCM {
+            // set additional authenticated data before doing real jobs.
+            if self.ctx.as_mut().unwrap().aad_set == false {
+                if let Some(aad) = &self.aad {
+                    self.ctx.as_mut().unwrap().ctx.aad_update(aad).unwrap();
+                    self.ctx.as_mut().unwrap().aad_set = true;
+                }
+            }
+        }
+
+        // do real jobs.
+        // this Crypter::update returns a Result<usize, ErrorStack>, we simply ignore the detailed
+        // error information by unwrapping it.
+        // we also can't use the question mark operatior since the error codes are differently
+        // defined in RustyVault and underlying adaptor, such as rust-openssl.
+        let count = self.ctx.as_mut().unwrap().ctx.update(&plaintext, &mut ciphertext[..]).unwrap();
+        Ok(count)
+    }
+
+    fn encrypt_final(&mut self, ciphertext: &mut Vec<u8>
+        ) -> Result<usize, RvError> {
+        // Unlike encrypt_update() function, we don't do auto-initialization here.
+        if let None = self.ctx {
+            return Err(RvError::ErrCryptoCipherNotInited);
+        }
+
+        let count = self.ctx.as_mut().unwrap().ctx.finalize(ciphertext).unwrap();
+
+        if self.alg.1 == CipherMode::GCM {
+            // set tag for caller to obtain.
+            if let Some(_) = self.tag {
+                // tag should not be set before encrypt_final() is called.
+                return Err(RvError::ErrCryptoCipherAEADTagPresent);
+            }
+
+            // 16-byte long is enough for all types of AEAD cipher tag.
+            // TODO: this is for AES-128-GCM only.
+            let mut tag: Vec<u8> = vec![0; 16];
+            self.ctx.as_mut().unwrap().ctx.get_tag(&mut tag).unwrap();
+            self.tag = Some(tag);
+        }
+
+        Ok(count)
+    }
+
+    fn decrypt(&mut self, ciphertext: &Vec<u8>) -> Result<Vec<u8>, RvError> {
+        match self.alg {
+            (AESKeySize::AES128, CipherMode::CBC) => {
+                let plaintext = decrypt(
+                    Cipher::aes_128_cbc(),
+                    &self.key,
+                    Some(&self.iv),
+                    ciphertext).unwrap();
+                return Ok(plaintext.to_vec());
+            }
+            (AESKeySize::AES128, CipherMode::GCM) => {
+                // aes_128_gcm's tag is 16-bytes long.
+                let plaintext = decrypt_aead(
+                    Cipher::aes_128_gcm(),
+                    &self.key,
+                    Some(&self.iv),
+                    &self.aad.clone().unwrap(),
+                    ciphertext,
+                    &self.tag.clone().unwrap()
+                    ).unwrap();
+                return Ok(plaintext.to_vec());
+            }
+            _ => Err(RvError::ErrCryptoCipherOPNotSupported),
+        }
+
+    }
+
+    fn decrypt_update(&mut self, ciphertext: Vec<u8>, plaintext: &mut Vec<u8>
+        ) -> Result<usize, RvError> {
+        let cipher;
+
+        match self.alg {
+            (AESKeySize::AES128, CipherMode::CBC) => {
+                cipher = Cipher::aes_128_cbc();
+            }
+            (AESKeySize::AES128, CipherMode::GCM) => {
+                cipher = Cipher::aes_128_gcm();
+            }
+            _ => { return Err(RvError::ErrCryptoCipherOPNotSupported); }
+        }
+
+        if let None = self.ctx {
+            // init adaptor ctx if it's not inited.
+            let encrypter = Crypter::new(
+                cipher,
+                Mode::Decrypt,
+                &self.key,
+                Some(&self.iv)
+            ).unwrap();
+            let adaptor_ctx = AdaptorCTX { ctx: encrypter, tag_set: false, aad_set: false };
+
+            self.ctx = Some(adaptor_ctx);
+        }
+
+        // set additional authenticated data before doing real jobs.
+        if self.alg.1 == CipherMode::GCM {
+            if self.ctx.as_mut().unwrap().aad_set == false {
+                if let Some(aad) = &self.aad {
+                    self.ctx.as_mut().unwrap().ctx.aad_update(aad).unwrap();
+                    self.ctx.as_mut().unwrap().aad_set = true;
+                }
+            }
+        }
+
+        // do real jobs.
+        // this Crypter::update returns a Result<usize, ErrorStack>, print detailed error if any.
+        match self.ctx.as_mut().unwrap().ctx.update(&ciphertext, plaintext) {
+            Ok(count) => { return Ok(count); }
+            Err(err_stack) => {
+                let errs = err_stack.errors();
+                println!("{}", errs.len());
+                for err in errs.iter() {
+                    println!("{:?}", err.reason());
+                }
+                Err(RvError::ErrCryptoCipherUpdateFailed)
+            }
+        }
+    }
+
+    fn decrypt_final(&mut self, plaintext: &mut Vec<u8>
+        ) -> Result<usize, RvError> {
+        // Unlike decrypt_update() function, we don't do auto-initialization here.
+        if let None = self.ctx {
+            return Err(RvError::ErrCryptoCipherNotInited);
+        }
+
+        // set tag before doing real jobs.
+        if self.alg.1 == CipherMode::GCM {
+            if self.ctx.as_mut().unwrap().tag_set == false {
+                if let Some(tag) = &self.tag {
+                    self.ctx.as_mut().unwrap().ctx.set_tag(tag).unwrap();
+                    self.ctx.as_mut().unwrap().tag_set = true;
+                } else {
+                    // if tag is missing, then return an error.
+                    return Err(RvError::ErrCryptoCipherNoTag);
+                }
+            }
+        }
+
+        match self.ctx.as_mut().unwrap().ctx.finalize(plaintext) {
+            Ok(count) => { return Ok(count); }
+            Err(err_stack) => {
+                let errs = err_stack.errors();
+                println!("{}", errs.len());
+                for err in errs.iter() {
+                    println!("{:?}", err.reason());
+                }
+                Err(RvError::ErrCryptoCipherFinalizeFailed)
+            }
+        }
+    }
+}
+
+impl AEADCipher for AES {
+    fn set_aad(&mut self, aad: Vec<u8>) -> Result<(), RvError> {
+        self.aad = Some(aad.clone());
+        Ok(())
+    }
+    fn get_tag(&mut self) -> Result<Vec<u8>, RvError> {
+        if self.tag == None {
+            return Err(RvError::ErrCryptoCipherNoTag);
+        }
+        Ok(self.tag.clone().unwrap())
+    }
+    fn set_tag(&mut self, tag: Vec<u8>) -> Result<(), RvError> {
+        self.tag = Some(tag.clone());
+        Ok(())
+    }
+}
